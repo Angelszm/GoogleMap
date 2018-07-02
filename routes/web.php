ff95f88525d7afce392c8048b8137beab0794e44
Angelszm <30867160+Angelszm@users.noreply.github.com>
2018-07-02 11:37:51 -0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use Cornford\Googlmapper\Facades\MapperFacade;
+use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+   Mapper::map(
+   	53.381128999999990000, 
+   	-1.470085000000040000,
+  [
+  	'zoom' => 16,
+    'draggable' => true,
+    'marker' => true,
+    'eventAfterLoad' => 'circleListener(maps[0].shapes[0].circle_0);'
+]
+   );
+
+   return view('welcome');
+});
+
+
+
